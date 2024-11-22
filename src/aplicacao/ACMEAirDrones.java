80fe290a7643ee2fb164f2223e2a310f77d8bfe1
Gabriel Domingues <gabrielleao.gd@gmail.com>
2024-11-22 16:40:39 -0300

    Inicio da funcionalidade Salvar dados

--- src/aplicacao/ACMEAirDrones.java
+++ src/aplicacao/ACMEAirDrones.java
@@ -20,82 +20,10 @@ public class ACMEAirDrones {
 
     public ACMEAirDrones() {
         drones = new Drones();
-        try {
-            Path path = Paths.get("saida.csv");
-            BufferedWriter bw = Files.newBufferedWriter(path, Charset.defaultCharset());
-            writer = new PrintWriter(bw);
-        } catch (Exception e) {
-            writer.printf("Erro ao criar o arquivo: " + e.getMessage());
-        }
     }
     public void executar(){
-        Locale.setDefault(Locale.ENGLISH);
-        leLinhasTexto();
         new JanelaCadastroDP(drones);
-        fecharArquivo();
     }
-
-    public void leLinhasTexto() {
-        Path path = Paths.get("entrada.csv");
-        try (BufferedReader reader = Files.newBufferedReader(path, Charset.defaultCharset())) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                if (line.startsWith("tipo")) {
-                    continue; // Ignora o cabeçalho
-                }
-                cadastrarDrone(line);
-            }
-        } catch (Exception e) {
-            writer.printf("Erro de E/S: %s%n", e);
-        }
-    }
-
-    public void cadastrarDrone(String line) {
-        try {
-            String[] dados = line.split(";");
-            int tipo = Integer.parseInt(dados[0]);
-            int codigo = Integer.parseInt(dados[1]);
-
-            if (tipo == 1) {
-                // Drone Pessoal
-                double custoFixo = Double.parseDouble(dados[2]);
-                double autonomia = Double.parseDouble(dados[3]);
-                int quantidadeMaximaPessoas  = Integer.parseInt(dados[4]);
-                DronePessoal dronePessoal = new DronePessoal(codigo, custoFixo, autonomia, quantidadeMaximaPessoas);
-                drones.addDrone(dronePessoal);
-                writer.println(dronePessoal.geraCSV());
-            } else if (tipo == 2) {
-                // Drone de Carga Inanimada
-                double custoFixo = Double.parseDouble(dados[2]);
-                double autonomia = Double.parseDouble(dados[3]);
-                double pesoMaximo  = Double.parseDouble(dados[4]);
-                boolean protecao = Boolean.parseBoolean(dados[5]);
-                DroneCargaInanimada droneCargaInanimada = new DroneCargaInanimada(codigo, custoFixo, autonomia, pesoMaximo,protecao);
-                drones.addDrone(droneCargaInanimada);
-                writer.println(droneCargaInanimada.geraCSV());
-            } else if (tipo == 3) {
-                //Drone de Carva Viva
-                double custoFixo = Double.parseDouble(dados[2]);
-                double autonomia = Double.parseDouble(dados[3]);
-                double pesoMaximo  = Double.parseDouble(dados[4]);
-                boolean climatizado = Boolean.parseBoolean(dados[5]);
-                DroneCargaViva droneCargaViva = new DroneCargaViva(codigo, custoFixo, autonomia, pesoMaximo,climatizado);
-                drones.addDrone(droneCargaViva);
-                writer.println(droneCargaViva.geraCSV());
-
-            }
-        } catch (ArrayIndexOutOfBoundsException e) {
-            writer.printf("Erro: Dados insuficientes na linha '%s'%n", line);
-        } catch (NumberFormatException e) {
-            writer.printf("Erro: Formato numérico inválido na linha '%s'%n", line);
-        } catch (Exception e) {
-            writer.printf("Erro inesperado ao processar a linha '%s': %s%n", line, e.getMessage());
-        }
-    }
-    private void fecharArquivo() {
-        writer.close();
-    }
-
 }
 
 
--- src/dados/TelaDadosES.java
+++ src/dados/TelaDadosES.java
@@ -1,12 +1,109 @@
 package dados;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Locale;
 
 public class TelaDadosES {
-    private JTextArea textArea1;
+    private JTextArea areaTexto;
     private JButton leituraSimulaçãoButton;
     private JButton salvarDadosButton;
     private JButton carregarDadosButton;
-    private JTextField textField1;
+    private JTextField nomeArquivo;
     private JButton voltar;
+    private PrintWriter writer;
+    private Drones drones;
+
+    public TelaDadosES() {
+        areaTexto.setEditable(false);
+        areaTexto.setLineWrap(true);        // Habilita a quebra de linha automática
+        areaTexto.setWrapStyleWord(true);   // Faz com que a quebra seja entre palavras
+        Locale.setDefault(Locale.ENGLISH);
+        drones = new Drones();
+
+        salvarDadosButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nome = nomeArquivo.getText();
+                try {
+                    Path path = Paths.get(nome+".csv");
+                    BufferedWriter bw = Files.newBufferedWriter(path, Charset.defaultCharset());
+                    writer = new PrintWriter(bw);
+                } catch (Exception exception) {
+                    writer.printf("Erro ao criar o arquivo: " + exception.getMessage());
+                }
+                //leLinhasTexto();
+                fecharArquivo();
+            }
+        });
+    }
+    private void fecharArquivo() {
+        writer.close();
+    }
+
+    public void SalvarDrone(String line) {
+        try {
+            String[] dados = line.split(";");
+            int tipo = Integer.parseInt(dados[0]);
+            int codigo = Integer.parseInt(dados[1]);
+
+            if (tipo == 1) {
+                // Drone Pessoal
+                double custoFixo = Double.parseDouble(dados[2]);
+                double autonomia = Double.parseDouble(dados[3]);
+                int quantidadeMaximaPessoas  = Integer.parseInt(dados[4]);
+                DronePessoal dronePessoal = new DronePessoal(codigo, custoFixo, autonomia, quantidadeMaximaPessoas);
+                drones.addDrone(dronePessoal);
+                writer.println(dronePessoal.geraCSV());
+            } else if (tipo == 2) {
+                // Drone de Carga Inanimada
+                double custoFixo = Double.parseDouble(dados[2]);
+                double autonomia = Double.parseDouble(dados[3]);
+                double pesoMaximo  = Double.parseDouble(dados[4]);
+                boolean protecao = Boolean.parseBoolean(dados[5]);
+                DroneCargaInanimada droneCargaInanimada = new DroneCargaInanimada(codigo, custoFixo, autonomia, pesoMaximo,protecao);
+                drones.addDrone(droneCargaInanimada);
+                writer.println(droneCargaInanimada.geraCSV());
+            } else if (tipo == 3) {
+                //Drone de Carva Viva
+                double custoFixo = Double.parseDouble(dados[2]);
+                double autonomia = Double.parseDouble(dados[3]);
+                double pesoMaximo  = Double.parseDouble(dados[4]);
+                boolean climatizado = Boolean.parseBoolean(dados[5]);
+                DroneCargaViva droneCargaViva = new DroneCargaViva(codigo, custoFixo, autonomia, pesoMaximo,climatizado);
+                drones.addDrone(droneCargaViva);
+                writer.println(droneCargaViva.geraCSV());
+
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            writer.printf("Erro: Dados insuficientes na linha '%s'%n", line);
+        } catch (NumberFormatException e) {
+            writer.printf("Erro: Formato numérico inválido na linha '%s'%n", line);
+        } catch (Exception e) {
+            writer.printf("Erro inesperado ao processar a linha '%s': %s%n", line, e.getMessage());
+        }
+    }
+
+    public void leLinhasTexto() {
+        Path path = Paths.get("entrada.csv");
+        try (BufferedReader reader = Files.newBufferedReader(path, Charset.defaultCharset())) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("tipo")) {
+                    continue; // Ignora o cabeçalho
+                }
+                SalvarDrone(line);
+            }
+        } catch (Exception e) {
+            writer.printf("Erro de E/S: %s%n", e);
+        }
+    }
 }
