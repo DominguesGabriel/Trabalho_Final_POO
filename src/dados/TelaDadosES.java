e7f5c898cea862bc97c4ae4ebce1beab17626b19
William de Oliveira Klein <klein.william.contato@gmail.com>
2024-11-22 15:59:47 -0300

    Tela principal e TelaDadosES

---
+++ src/dados/TelaDadosES.java
@@ -0,0 +1,12 @@
+package dados;
+
+import javax.swing.*;
+
+public class TelaDadosES {
+    private JTextArea textArea1;
+    private JButton leituraSimulaçãoButton;
+    private JButton salvarDadosButton;
+    private JButton carregarDadosButton;
+    private JTextField textField1;
+    private JButton voltar;
+}
---
+++ src/dados/TelaPrincipal.java
@@ -0,0 +1,15 @@
+package dados;
+
+import javax.swing.*;
+
+public class TelaPrincipal {
+    private JButton cadastrarTransporteButton;
+    private JButton cadastrarDronePessoalButton;
+    private JButton cadastrarDroneCargaButton;
+    private JButton dadosESButton;
+    private JButton relatórioGeralButton;
+    private JButton mostrarTransportesButton;
+    private JButton gerenciarTransportesButton;
+    private JButton sairButton;
+    private JTextArea textArea1;
+}
