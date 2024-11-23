4a1e01bbd552c7d6debdb21640e6de2b19282f41
William de Oliveira Klein <klein.william.contato@gmail.com>
2024-11-23 09:35:02 -0300

    GUI GerenciamentoTransporte

---
+++ src/dados/TelaGerenciamentoTransporte.java
@@ -0,0 +1,11 @@
+package dados;
+
+import javax.swing.*;
+
+public class TelaGerenciamentoTransporte {
+    private JPanel panel1;
+    private JButton processarButton;
+    private JTextField textField1;
+    private JButton alterarSituaçãoButton;
+    private JButton voltarButton;
+}
