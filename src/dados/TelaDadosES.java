a26f3b2625afd2b9e183dc0f7d536e5dc72c19c9
Gabriel Domingues <gabrielleao.gd@gmail.com>
2024-11-27 06:01:40 -0300

    Tela de ES finalizada.

--- src/dados/TelaDadosES.java
+++ src/dados/TelaDadosES.java
@@ -118,10 +118,28 @@ public class TelaDadosES {
         leituraSimulaçãoButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                String nome = nomeArquivo.getText().trim();
+                if (nome.isEmpty()) {
+                    areaTexto.setForeground(Color.RED);
+                    areaTexto.setText("Erro: Por favor, insira o nome do arquivo.\n");
+                    return;
+                }
+                if (!nome.matches("[a-zA-Z0-9._-]+")) {
+                    areaTexto.setForeground(Color.RED);
+                    areaTexto.setText("Erro: O nome do arquivo contém caracteres inválidos.\n");
+                    return;
+                }
+                //Path path = Paths.get(nome + ".csv");
+                leLinhasSimulacao(nome + ".csv");
+                areaTexto.setForeground(Color.GREEN);
+                areaTexto.setText("Dados carregados com sucesso no arquivo: " + nome + ".csv\n");
+                areaTexto.setForeground(Color.black);
+                areaTexto.append(imprimirFilaTransporte());
+                areaTexto.append(drones.imprimirDrones());
             }
         });
     }
+
     private void fecharArquivo() {
         if (writer != null) {
             writer.close();
@@ -129,7 +147,87 @@ public class TelaDadosES {
         }
     }
 
-    public void SalvarDados(String line) {
+    public void carregarTransportesFila(String line){
+        try {
+            String[] dados = line.split(";");
+            int tipo = Integer.parseInt(dados[0]);
+            int codigo = Integer.parseInt(dados[1]);
+
+            if (tipo == 1 && dados.length == 5) {
+                // Drone Pessoal
+                double custoFixo = Double.parseDouble(dados[2]);
+                double autonomia = Double.parseDouble(dados[3]);
+                int quantidadeMaximaPessoas = Integer.parseInt(dados[4]);
+                DronePessoal dronePessoal = new DronePessoal(codigo, custoFixo, autonomia, quantidadeMaximaPessoas);
+                drones.addDrone(dronePessoal);
+            } else if (tipo == 2 && dados.length == 6) {
+                // Drone de Carga Inanimada
+                double custoFixo = Double.parseDouble(dados[2]);
+                double autonomia = Double.parseDouble(dados[3]);
+                double pesoMaximo = Double.parseDouble(dados[4]);
+                boolean protecao = Boolean.parseBoolean(dados[5]);
+                DroneCargaInanimada droneCargaInanimada = new DroneCargaInanimada(codigo, custoFixo, autonomia, pesoMaximo, protecao);
+                drones.addDrone(droneCargaInanimada);
+            } else if (tipo == 3 && dados.length == 6) {
+                // Drone de Carga Viva
+                double custoFixo = Double.parseDouble(dados[2]);
+                double autonomia = Double.parseDouble(dados[3]);
+                double pesoMaximo = Double.parseDouble(dados[4]);
+                boolean climatizado = Boolean.parseBoolean(dados[5]);
+                DroneCargaViva droneCargaViva = new DroneCargaViva(codigo, custoFixo, autonomia, pesoMaximo, climatizado);
+                drones.addDrone(droneCargaViva);
+            }
+            else if (tipo == 1 && dados.length == 10) {
+                //transporte pessoal
+                String nomeCliente = dados[2];
+                String descricao = dados[3];
+                double peso = Double.parseDouble(dados[4]);
+                double latitudeOrigem = Double.parseDouble(dados[5]);
+                double latitudeDestino = Double.parseDouble(dados[6]);
+                double longitudeOrigem = Double.parseDouble(dados[7]);
+                double longitudeDestino = Double.parseDouble(dados[8]);
+                int quantidadePessoas = Integer.parseInt(dados[9]);
+                TransportePessoal transportePessoal = new TransportePessoal(codigo,nomeCliente,descricao,peso,latitudeOrigem,latitudeDestino,longitudeOrigem,longitudeDestino,quantidadePessoas);
+                transportes.addTransporte(transportePessoal);
+                filaTransportes.add(transportePessoal);
+            }else if(tipo == 2 && dados.length == 10) {
+                //transporte carga inanimada
+                String nomeCliente = dados[2];
+                String descricao = dados[3];
+                double peso = Double.parseDouble(dados[4]);
+                double latitudeOrigem = Double.parseDouble(dados[5]);
+                double latitudeDestino = Double.parseDouble(dados[6]);
+                double longitudeOrigem = Double.parseDouble(dados[7]);
+                double longitudeDestino = Double.parseDouble(dados[8]);
+                boolean cargaPerigosa = Boolean.parseBoolean(dados[9]);
+                TransporteCargaInanimada transporteCargaInanimada = new TransporteCargaInanimada(codigo,nomeCliente,descricao,peso,latitudeOrigem,latitudeDestino,longitudeOrigem,longitudeDestino,cargaPerigosa);
+                transportes.addTransporte(transporteCargaInanimada);
+                filaTransportes.add(transporteCargaInanimada);
+            }else if(tipo == 3 && dados.length == 11) {
+                //transporte carga viva
+                String nomeCliente = dados[2];
+                String descricao = dados[3];
+                double peso = Double.parseDouble(dados[4]);
+                double latitudeOrigem = Double.parseDouble(dados[5]);
+                double latitudeDestino = Double.parseDouble(dados[6]);
+                double longitudeOrigem = Double.parseDouble(dados[7]);
+                double longitudeDestino = Double.parseDouble(dados[8]);
+                double temperaturaMinima = Double.parseDouble(dados[9]);
+                double temperaturaMaxima = Double.parseDouble(dados[10]);
+                TransporteCargaViva transporteCargaViva = new TransporteCargaViva(codigo,nomeCliente,descricao,peso,latitudeOrigem,latitudeDestino,longitudeOrigem,longitudeDestino,temperaturaMinima,temperaturaMaxima);
+                transportes.addTransporte(transporteCargaViva);
+                filaTransportes.add(transporteCargaViva);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            areaTexto.setText("Erro: Dados insuficientes na linha '" + line + "'\n");
+        } catch (NumberFormatException e) {
+            areaTexto.setText("Erro: Formato numérico inválido na linha '" + line + "'\n");
+        } catch (Exception e) {
+            areaTexto.setText("Erro inesperado ao processar a linha '" + line + "': " + e.getMessage() + "\n");
+        }
+    }
+
+    public void salvarDados(String line) {
         try {
             String[] dados = line.split(";");
             int tipo = Integer.parseInt(dados[0]);
@@ -206,7 +304,6 @@ public class TelaDadosES {
         }
     }
 
-
     public void leLinhasTexto(String nomeArquivo) {
         Path path = Paths.get(nomeArquivo);
         try (BufferedReader reader = Files.newBufferedReader(path, Charset.defaultCharset())) {
@@ -215,7 +312,23 @@ public class TelaDadosES {
                 if (line.startsWith("tipo")) {
                     continue; // Ignora o cabeçalho
                 }
-                SalvarDados(line);
+                salvarDados(line);
+            }
+        } catch (Exception e) {
+            areaTexto.setForeground(Color.RED);
+            areaTexto.setText("Erro ao carregar os dados do arquivo: " + e.getMessage() + "\n");
+        }
+    }
+
+    public void leLinhasSimulacao(String nomeArquivo) {
+        Path path = Paths.get(nomeArquivo);
+        try (BufferedReader reader = Files.newBufferedReader(path, Charset.defaultCharset())) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("tipo")) {
+                    continue; // Ignora o cabeçalho
+                }
+                carregarTransportesFila(line);
             }
         } catch (Exception e) {
             areaTexto.setForeground(Color.RED);
@@ -225,6 +338,27 @@ public class TelaDadosES {
 
     public JPanel getPainel() {
         return painel;
+    }
+
+    private String imprimirFilaTransporte(){
+        StringBuilder sb = new StringBuilder();
+
+        if(filaTransportes.isEmpty()) {
+            return "Não há transportes cadastrados.";
+        }
+
+        for(Transporte t : filaTransportes){
+            if(t instanceof TransportePessoal){
+                sb.append(((TransportePessoal)t).toString()).append("\n");
+            }
+            else if(t instanceof TransporteCargaInanimada){
+                sb.append(((TransporteCargaInanimada)t).toString()).append("\n");
+            }
+            else if (t instanceof TransporteCargaViva) {
+                sb.append(((TransporteCargaViva)t).toString()).append("\n");
+            }
+        }
+        return sb.toString();
     }
 
 }
